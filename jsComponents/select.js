34f860dfc9406c6044b49cad38621fe1cbaacd4a
cfurlotte <alowtron@gmail.com>
2024-01-26 00:14:31 -0500

    updates select.js

--- jsComponents/select.js
+++ jsComponents/select.js
@@ -1,12 +1,17 @@
 function SelectedCreatureType(selectedCreatureType) {
-    document.getElementById("idCreatureType2").innerHTML = `
-
-    <h3>Land/Water/Air</h3>
-    <select name="idCreatureType2" title="idCreatureType2" oninput="">
-        <option value="random">Random</option>
-        <option value="land">Land</option>
-        <option value="later">Water</option>
-        <option value="air">Air</option>
-    </select>
-    `
+    if (selectedCreatureType == "random") {
+        document.getElementById("idCreatureType2").innerHTML = ''
+    } else {
+        document.getElementById("idCreatureType2").innerHTML = 
+        `
+        <h3>Land/Water/Air</h3>
+        <select name="idCreatureType2" title="idCreatureType2" oninput="">
+            <option value="random">Random</option>
+            <option value="land">Land</option>
+            <option value="later">Water</option>
+            <option value="air">Air</option>
+        </select>
+        `
+    }
+    
 }
