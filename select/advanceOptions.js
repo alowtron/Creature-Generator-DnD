6d5b40fa80f568539aaab5eea071a2135e9dab30
cfurlotte <alowtron@gmail.com>
2024-01-28 21:15:08 -0500

    set up the window.confirm

--- select/advanceOptions.js
+++ select/advanceOptions.js
@@ -1,11 +1,16 @@
 function AdvanceOptions(onOf) {
     if (onOf == 'true') {
-        window.confirm()
-        document.getElementById('divAdvanceOptionsToggle').innerHTML = `
+        let leave = window.confirm('Are you sure you want to disable? Options selected will be lost.')
+        if (leave) {
+            document.getElementById('divAdvanceOptionsToggle').innerHTML = `
         <button onclick="AdvanceOptions(value)" value="false">
             Toggle Advance Options
         </button>`
         document.getElementById('divAdvanceOptions').innerHTML = `<div id="idAdvanceOptions"></div>`
+        } else {
+            return
+        }
+        
     } else {
         
         document.getElementById('divAdvanceOptionsToggle').innerHTML = `
